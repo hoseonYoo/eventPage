ffabbe544cf06a2aee0e268c733947a2581f01a9
hoseonYoo <hohosun2i@gmail.com>
2024-08-07 21:07:36 +0900

    Initial commit

---
+++ back/src/main/java/com/pagemaker/evnet/EvnetApplication.java
@@ -0,0 +1,13 @@
+package com.pagemaker.evnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvnetApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/pagemaker/evnet/EvnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagemaker.evnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
